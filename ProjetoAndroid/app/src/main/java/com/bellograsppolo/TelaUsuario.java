25b8db3e267322a1eb657976c4c92639a8e06aa6
DallAgnol1996 <32672343+DallAgnol1996@users.noreply.github.com>
2021-08-21 16:34:20 -0300

    TelaLogin1
    Tela de Login v1

---
+++ ProjetoAndroid/app/src/main/java/com/bellograsppolo/TelaUsuario.java
@@ -0,0 +1,14 @@
+package com.bellograsppolo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_usuario);
+        getSupportActionBar().hide();
+    }
+}
